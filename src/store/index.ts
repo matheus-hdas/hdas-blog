432ff7cf242603c43781ea60676f942e2699b62c
matheus-hdas <matheussantos.betim@gmail.com>
2025-12-10 19:15:01 -0300

    style: change folders name to plural

--- src/store/index.ts
+++
@@ -1,3 +0,0 @@
-export { useThemeStore } from "@/store/theme.store";
-export { useBlogStore } from "@/store/blog.store";
-export { useBlogAdminStore } from "@/store/blog.admin.store";
---
+++ src/stores/index.ts
@@ -0,0 +1,3 @@
+export { useThemeStore } from "@/stores/theme.store";
+export { useBlogStore } from "@/stores/blog.store";
+export { useBlogAdminStore } from "@/stores/blog.admin.store";
