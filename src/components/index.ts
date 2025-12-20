2816cbf6facff8a94de3638ca90fa8db6700950f
matheus-hdas <matheussantos.betim@gmail.com>
2025-12-19 23:00:27 -0300

    chore: add `NewBlogPost` component

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,3 +2,4 @@ export { LocaleDropdown } from "@/components/locale.dropdown";
 export { ToggleTheme } from "@/components/toggle.theme";
 export { AdminHero } from "@/components/admin.hero";
 export { NewBlog } from "@/components/new.blog";
+export { NewBlogPost } from "@/components/new.blog.post";
