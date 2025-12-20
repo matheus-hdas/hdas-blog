ef570434e5609d1852011fa6b5e91054d9009875
matheus-hdas <matheussantos.betim@gmail.com>
2025-12-19 23:01:06 -0300

    chore: add `EditBlogPost` component

--- src/components/index.ts
+++ src/components/index.ts
@@ -3,3 +3,4 @@ export { ToggleTheme } from "@/components/toggle.theme";
 export { AdminHero } from "@/components/admin.hero";
 export { NewBlog } from "@/components/new.blog";
 export { NewBlogPost } from "@/components/new.blog.post";
+export { EditBlogPost } from "@/components/edit.blog.post";
